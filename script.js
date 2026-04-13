616b5d455e2032da1f7877ada2ae80fa688fb19f
Gonzalo Castillo <gonzalocastilloar@gmail.com>
2026-04-13 12:31:32 -0300

    init

--- script.js
+++ script.js
@@ -9,16 +9,66 @@ var esDescanso = false;
 var esDescansoLargo = false;
 var contDescansos = 0;
 var contFocos = 0;
-var clock = null;
+var isRunning = false;
+var isFirstRun = true;
+
+// DOMS
+var clockDom = null;
+var button1Dom = false;
+var button2Dom = false;
+
+function StartButton() {
+  if (isRunning) {
+    clearInterval(timer);
+    ShowClock();
+    button1Dom.innerHTML = 'Iniciar';
+    isRunning = false;
+  } else {
+    isRunning = true;
+    button1Dom.innerHTML = 'Pausar';
+    if (isFirstRun) {
+      sec = 59;
+      min = 24;
+      isFirstRun = false;
+    }
+    Start();
+  }
+  button1Dom.classList.toggle('active');
+  button2Dom.classList.toggle('active');
+}
 
 function Start() {
   if (timer) {
     clearInterval(timer);
   }
   timer = setInterval(Timer, 1000);
-  clock = document.getElementById('clock');
   End();
+  ShowClock();
 }
+
+function ShowClock() {
+  clockDom.innerHTML =
+    `${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`
+}
+
+function Stop() {
+  clearInterval(timer);
+  isRunning = false;
+  button1Dom.innerHTML = 'Iniciar';
+  button1Dom.classList.remove('active');
+  button2Dom.classList.remove('active');
+  sec = 0;
+  min = 0;
+  ShowClock();
+  RestartDoms();
+}
+
+function RestartDoms() {
+  document.getElementById('descansos').classList.remove('active');
+  document.getElementById('descansosLargos').classList.remove('active');
+  document.getElementById('focos').classList.remove('active');
+}
+
 
 function End() {
   if (esFoco === true) {
@@ -31,13 +81,17 @@ function End() {
     descansosLargos++;
   }
 
+  document.getElementById('descansos').classList.remove('active');
+  document.getElementById('descansosLargos').classList.remove('active');
+  document.getElementById('focos').classList.remove('active');
+
   sec = 0;
   if (esDescansoLargo) {
     min = 25;
     esFoco = true;
     esDescanso = false;
     esDescansoLargo = false;
-    document.getElementById('evento').innerHTML = 'Foco';
+    document.getElementById('focos').classList.add('active');
   } else if (contDescansos == 2 && contFocos == 3) {
     contDescansos = 0;
     contFocos = 0;
@@ -45,23 +99,24 @@ function End() {
     esFoco = false;
     esDescanso = false;
     esDescansoLargo = true;
-    document.getElementById('evento').innerHTML = 'Descanso largo';
+    document.getElementById('descansosLargos').classList.add('active');
   } else if (esFoco) {
     min = 5;
     esFoco = false;
     esDescanso = true;
     esDescansoLargo = false;
-    document.getElementById('evento').innerHTML = 'Descanso';
+    document.getElementById('descansos').classList.add('active');
   } else {
     min = 25;
     esFoco = true;
     esDescanso = false;
     esDescansoLargo = false;
-    document.getElementById('evento').innerHTML = 'Foco';
+    document.getElementById('focos').classList.add('active');
   }
   document.getElementById('focos').innerHTML = focos + ' focos';
   document.getElementById('descansos').innerHTML = descansos + ' descansos';
   document.getElementById('descansosLargos').innerHTML = descansosLargos + ' descansos largos';
+  Save();
 }
 
 function Timer() {
@@ -79,14 +134,63 @@ function Timer() {
   ShowClock();
 }
 
-function ShowClock() {
-  clock.innerHTML =
-    `${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`
+function Save() {
+  let obj = {
+    sec,
+    min,
+    focos,
+    descansos,
+    descansosLargos,
+    esFoco,
+    esDescanso,
+    esDescansoLargo,
+    contDescansos,
+    contFocos
+  };
+
+  window.localStorage.setItem('history', JSON.stringify(obj));
 }
 
-function Stop() {
-  clearInterval(timer);
-  sec = 0;
-  min = 0;
-  ShowClock();
+function Reset() {
+  if (confirm("¿Seguro desea eliminar todos los registros?")) {
+    sec = 0;
+    min = 0;
+    focos = 0;
+    descansos = 0;
+    descansosLargos = 0;
+    esFoco = false;
+    esDescanso = false;
+    esDescansoLargo = false;
+    contDescansos = 0;
+    contFocos = 0;
+    ShowClock();
+    Save();
+    RestartDoms();
+    clearInterval(timer);
+  }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  // set doms
+  clockDom = document.getElementById('clock');
+  button1Dom = document.getElementById('button1');
+  button2Dom = document.getElementById('button2');
+
+  // load history
+  let data = window.localStorage.getItem('history');
+
+  if (data) {
+    data = JSON.parse(data);
+    sec = data.sec;
+    min = data.min;
+    focos = data.focos;
+    descansos = data.descansos;
+    descansosLargos = data.descansosLargos;
+    esFoco = data.esFoco;
+    esDescansoLargo = data.esDescansoLargo;
+    esDescanso = data.esDescanso;
+    contDescansos = data.contDescansos;
+    contFocos = data.contFocos;
+  }
+  ShowClock();
+})
